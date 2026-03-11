65aaa4a7a9e18a14c25611c28a78aeb0bad1dde8
jbritain <50422789+jbritain@users.noreply.github.com>
2026-03-11 10:15:32 +0000

    slime ethan

--- bots/examplebot.py
+++
@@ -1,6 +0,0 @@
-from logic import RockyPlayer, Move
-
-class StupidBot(RockyPlayer):
-    name = "Rocky"
-    image_path = "images/stupid.png"  # optional
-
--- main.py
+++ main.py
@@ -14,34 +14,47 @@ MATCHES = 1000
 # For development I recommend not processing in parallel as it can make it much harder to find errors
 PARALLEL = False
 
+
 class MyPlayer(Player):
-    name = 'Name your bot/Team name'
-    image_path = 'images/your_image.png' # Optional
+    name = "crAAcked"
+    image_path = "images/your_image.png"  # Optional
 
     def get_hand_type(self, community_cards: list[str]) -> HandRank:
         # Handle pre flop calls
         if not community_cards:
-            return HandRank.ONE_PAIR if self.cards[0][0] == self.cards[1][0] else HandRank.HIGH_CARD
+            return (
+                HandRank.ONE_PAIR
+                if self.cards[0][0] == self.cards[1][0]
+                else HandRank.HIGH_CARD
+            )
 
         rank = evaluate_cards(*community_cards, *self.cards)
         for hand_type in HandRank:
             if rank <= hand_type.value:
                 return hand_type
-        raise IndexError(f'Hand Rank Out Of Range: {rank}')
+        raise IndexError(f"Hand Rank Out Of Range: {rank}")
 
     def get_equity(self, community_cards: list[str], samples: int = 5000) -> float:
         """Placeholder equity calculation function. You do not have to implement a function like this but some sort of equity calculation is highly recommended."""
 
         return 0.0
 
-    def move(self, community_cards: list[str], valid_moves: list[Move], round_history: list[tuple[Move, int]], min_bet: int, max_bet: int) -> tuple[Move, int] | Move:
+    def move(
+        self,
+        community_cards: list[str],
+        valid_moves: list[Move],
+        round_history: list[tuple[Move, int]],
+        min_bet: int,
+        max_bet: int,
+    ) -> tuple[Move, int] | Move:
         """Your move code here! You are given the community cards (cards both players have access to, the objective is to use your 2 cards (self.cards) with the community cards to make the best 5-card poker hand).
         You are also given a list containing the legal moves you can currently make, for example, if the opponent has bet then you can only call, raise or fold but cannot check.
-        If your bot attempts to make an illegal move it will fold its hand (forfeiting any chips already in the pot), so ensure not to do this."""
+        If your bot attempts to make an illegal move it will fold its hand (forfeiting any chips already in the pot), so ensure not to do this.
+        """
 
-        
         # self.get_hand_type(community_cards) == HandRank.THREE_OF_A_KIND
         return Move.FOLD
+
 
 def run_match(_: int) -> str:
     """Run a single match and return the winner's name."""
@@ -49,16 +62,19 @@ def run_match(_: int) -> str:
     game = Game(p1, p2, debug=False)
     return game.simulate_hands().name
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     win_counts = Counter()
     # This runs the large number of matches in parallel, which drastically speeds up computation time
-    if (PARALLEL):
+    if PARALLEL:
         with Pool(cpu_count()) as pool:
             results = pool.map(run_match, range(MATCHES))
             win_counts.update(results)
     else:
         for i in range(MATCHES):
-            win_counts.update((run_match(i),)) 
+            win_counts.update((run_match(i),))
 
     player_name, wins = win_counts.most_common(1)[0]
-    print(f'{player_name} won the most with {wins}/{MATCHES} ({(wins / MATCHES) * 100:.2f}%)')
+    print(
+        f"{player_name} won the most with {wins}/{MATCHES} ({(wins / MATCHES) * 100:.2f}%)"
+    )
